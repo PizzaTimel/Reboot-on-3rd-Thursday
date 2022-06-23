79de80485c7f8af945ff58ec132700eec1c76baf
Frank Lange <f.lange90@googlemail.com>
2022-06-23 12:53:34 +0200

    prototype

--- Date.cs
+++ Date.cs
@@ -4,32 +4,27 @@ namespace dotnet6
     {        
         public static bool Checkdate()
         {
-            bool        TodayReboot;            
+            //Locate first Thursday of Month - add 2 weeks
             DateTime    Today               = System.DateTime.Now;
             DateTime    FirstDayOfMonth     = new DateTime(System.DateTime.Now.Year, System.DateTime.Now.Month, 1);
-            DateTime    CheckDay            = FirstDayOfMonth.AddDays(-1);
             int         Difference          = new int();      
             DateTime    ThursdayInTwoWeeks  = new DateTime();
             
             Difference = 14 + (4 - ((int)FirstDayOfMonth.DayOfWeek));
-            ThursdayInTwoWeeks = FirstDayOfMonth.AddDays(Difference);
-            //DateTime FirstDayOfMonth = new DateTime();
-            //Get First Day of Month - set week 1 as Array - get Thursday of Array - add 2 weeks - find Thursday
+            ThursdayInTwoWeeks = FirstDayOfMonth.AddDays(Difference);            
 
             Console.WriteLine("First Day of Month was : " + ((int)FirstDayOfMonth.DayOfWeek));
-            Console.WriteLine("Day before was : " + ((int)CheckDay.DayOfWeek));
-            Console.WriteLine("3rd Thrusday in this Month is: " + ThursdayInTwoWeeks);
+            Console.WriteLine("3rd Thursday in this Month is: " + ThursdayInTwoWeeks);
 
             if(Today == ThursdayInTwoWeeks)
             {
-                TodayReboot = true;
+                return true;
             }
             else
             {
-                TodayReboot = false;
+                return false;
             }
             
-            return TodayReboot;
         }
     }
 }
